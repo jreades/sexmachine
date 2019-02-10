df814ca919b955a62e1bc30c2cf3f0092a139aa9
Jon Reades <jon@reades.com>
2019-02-10 10:37:11 +0000

    Got __version__ working from __init__.py file.

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='SexMachine',
-    version='0.1.2',
+import sexmachine
+
+setuptools.setup(
+    name='sexmachine',
+    version=sexmachine.__version__,
     author='Jon Reades',
     author_email='jon@reades.com',
     packages=['sexmachine', 'sexmachine.test'],
     package_dir={'sexmachine': 'sexmachine'},
     package_data={'sexmachine': ['data/*.txt.gz']},
-    url='https://github.com/ferhatelmas/sexmachine/',
+    url='https://github.com/jreades/sexmachine',
     license='GPLv3',
-    description='Get the gender from first name.',
-    long_description=open('README.rst').read(),
+    description='Infer the gender from first name using data from gender.c library.',
+    long_description=open('README.rst','r').read(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
 )
